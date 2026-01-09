13c8951ca0439bdef810647438ecf2b4683c49fb
zexulz <zexulzs@gmail.com>
2026-01-09 08:47:06 -0800

    Update check.js

--- api/check.js
+++ api/check.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-    // Enable CORS
+    
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
